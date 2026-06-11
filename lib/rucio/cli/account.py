88b321c4f42489e56885869ed87a64ebd0bef903
Matti Jansson <karl.jansson@uclouvain.be>
2026-06-11 11:09:02 +0200

    Adding OIDC_ALL identity type to rucio admin identity command.

--- lib/rucio/cli/account.py
+++ lib/rucio/cli/account.py
@@ -222,7 +222,7 @@ def limit_list(ctx: click.Context, account_name: str, rse: Optional[str]):
 @limit.command("add")
 @click.argument(
     "account-name",
-)
+)add_identity
 @click.option("--rse", "--rse-name", help="Full RSE name", required=True)  # TODO Separate RSE (local) and RSE Expression (global)
 @click.option("--bytes", "bytes_", help='Value of the limit; can be specified in bytes ("10000"), with a storage unit ("10GB"), or "infinity"', required=True)
 @click.option("--locality", type=click.Choice(["local", "global"]), help="Global or local limit scope", default="local")
@@ -284,7 +284,7 @@ def identity_list(ctx: click.Context, account_name: str):
 
 @identity.command("add")
 @click.argument("account-name", required=True)
-@click.option("--type", "type_", type=click.Choice(["X509", "GSS", "USERPASS", "SSH", "SAML", "OIDC"]), help="Authentication type", required=True)
+@click.option("--type", "type_", type=click.Choice(["X509", "GSS", "USERPASS", "SSH", "SAML", "OIDC", "OIDC_ALL"]), help="Authentication type", required=True)
 @click.option("--id", help="Identity", required=True)
 @click.option("--email", help="Email address associated with the identity", required=True)
 @click.option("--password", help="Password if authtype is USERPASS")
@@ -303,7 +303,7 @@ def identity_add(ctx: click.Context, account_name: str, type_: str, id: str, ema
 
 @identity.command("remove")
 @click.argument("account-name", required=True)
-@click.option("--type", "type_", type=click.Choice(["X509", "GSS", "USERPASS", "SSH", "SAML", "OIDC"]), help="Authentication type", required=True)
+@click.option("--type", "type_", type=click.Choice(["X509", "GSS", "USERPASS", "SSH", "SAML", "OIDC", "OIDC_ALL"]), help="Authentication type", required=True)
 @click.option("--id", help="Identity", required=True)
 @click.pass_context
 def identity_remove(ctx: click.Context, account_name: str, type_: str, id: str):
